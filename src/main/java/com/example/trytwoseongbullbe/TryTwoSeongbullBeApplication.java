86358b27bbf47225933436d5befe21f5666fdcf3
김윤진(Kim YunJin) <yunjinkim25@mju.ac.kr>
2025-12-16 19:44:31 +0900

    chore: initial spring boot backend setup

---
+++ src/main/java/com/example/trytwoseongbullbe/TryTwoSeongbullBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.trytwoseongbullbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryTwoSeongbullBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryTwoSeongbullBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trytwoseongbullbe/TryTwoSeongbullBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trytwoseongbullbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryTwoSeongbullBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
