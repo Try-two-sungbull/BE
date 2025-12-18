db11b14b306e605d6a7263b1367a7c688477ceb1
김윤진(Kim YunJin) <yunjinkim25@mju.ac.kr>
2025-12-18 20:26:48 +0900

    fix: Swagger 로그인 예제 비밀번호 값 수정

--- src/main/java/com/example/trytwoseongbullbe/global/auth/dto/request/SignInRequest.java
+++ src/main/java/com/example/trytwoseongbullbe/global/auth/dto/request/SignInRequest.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record SignInRequest(
         @Schema(example = "admin")
         String username,
-        @Schema(example = "1234")
+        @Schema(example = "12345")
         String password
 ) {
 }
