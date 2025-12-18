eb3db7471e1e84425591658bc1c45c721382dab0
김윤진(Kim YunJin) <yunjinkim25@mju.ac.kr>
2025-12-18 15:52:03 +0900

    fix: 프론트엔드 접근을 위한 CORS 설정 추가

---
+++ src/main/java/com/example/trytwoseongbullbe/global/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.example.trytwoseongbullbe.global.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(
+                "https://be.hack.bluerack.org",
+                "https://localhost:8080",
+                "http://localhost:8080",
+                "http://localhost:3000"
+
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("Authorization"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
